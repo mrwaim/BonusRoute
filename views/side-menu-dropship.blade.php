c4f0e070109833252781df6e599a0024556de617
Achmad Fatoni <achmadfatony@gmail.com>
2016-05-31 11:55:48 +0800

    dropship can see their bonus

---
+++ views/side-menu-dropship.blade.php
@@ -0,0 +1,19 @@
+@if($config->has_bonus)
+    @include('elements.side-menu-parent-item', [
+    'folder' => 'bonus-management',
+    'menu' => 'Bonus',
+    'menuIcon' => 'fa-trophy',
+    'children' => [ [
+    'filter' => 'me',
+    'url' => 'list/me',
+    'menu' => 'Bonus Rewarded to You',
+    ], [
+    'filter' => 'down-line',
+    'url' => 'list/down-line',
+    'menu' => 'Bonus rewarded to your Downliner',
+    ], [
+    'page' => 'view',
+    'menu' => 'Bonus Details',
+    ]
+    ]])
+@endif
