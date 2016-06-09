e14d866f0152604e185604352699bee1577b5648
Achmad Fatoni <achmadfatony@gmail.com>
2016-06-09 09:34:53 +0700

    show bonus in organization for manager

--- views/side-menu-stockist.blade.php
+++ views/side-menu-stockist.blade.php
@@ -1,19 +1,35 @@
+<?php
+$children = [
+        [
+            'filter' => 'me',
+            'url' => 'list/me',
+            'menu' => 'Bonus Rewarded to You',
+        ],
+        [
+            'filter' => 'down-line',
+            'url' => 'list/down-line',
+            'menu' => 'Bonus rewarded to your Downliner',
+        ],
+        [
+            'page' => 'view',
+            'menu' => 'Bonus Details',
+        ]
+];
+
+if($auth->manager){
+    $children[] = [
+        'filter' => 'org',
+        'url' => 'list/org',
+        'menu' => 'Bonus In your organization',
+    ];
+}
+?>
+
 @if($config->has_bonus)
 @include('elements.side-menu-parent-item', [
 'folder' => 'bonus-management',
 'menu' => 'Bonus',
 'menuIcon' => 'fa-trophy',
-'children' => [ [
-'filter' => 'me',
-'url' => 'list/me',
-'menu' => 'Bonus Rewarded to You',
-], [
-'filter' => 'down-line',
-'url' => 'list/down-line',
-'menu' => 'Bonus rewarded to your Downliner',
-], [
-'page' => 'view',
-'menu' => 'Bonus Details',
-]
-]])
+'children' => $children,
+])
 @endif
