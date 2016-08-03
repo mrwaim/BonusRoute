2d47c4458a4847507d6d8c345c062817a7dfcef8
Achmad Fatoni <achmadfatony@gmail.com>
2016-08-03 11:04:25 +0800

    refinement on approve and reject all button (#11)

--- views/list-payments.blade.php
+++ views/list-payments.blade.php
@@ -93,20 +93,13 @@
             </div>
         </div>
     </section>
-    <div>
+    <div class="form">
         <form action="/bonus-management/set-approvals-all" method="post">
-            <button class="btn btn-success approve-all-bonuses" type="submit">Approve All</button>
+            {{ csrf_field() }}
             <input type="hidden" name="monthly_report_id" value="{{ $report }}">
             <input type="hidden" name="type" value="{{ $user_type }}">
-            <input type="hidden" name="approved_state" value="approve">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        </form>
-        <form action="/bonus-management/set-approvals-all" method="post">
-            <button class="btn btn-success approve-all-bonuses" type="submit">Reject all</button>
-            <input type="hidden" name="monthly_report_id" value="{{ $report }}">
-            <input type="hidden" name="type" value="{{ $user_type }}">
-            <input type="hidden" name="approved_state" value="reject">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <input class="btn btn-success" type="submit" name="approved_state" value="Approve All"/>
+            <input class="btn btn-success" type="submit" name="approved_state" value="Reject All"/>
         </form>
     </div>
 @endsection
